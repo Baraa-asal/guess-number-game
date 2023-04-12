da8859574dc995f5f139c0bdbb76de348869a2cd
baraa-asal <b.aboasal@gmail.com>
2023-04-12 10:22:14 +0200

    adding a feature to display the number after losing the game

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Generating a random number between 500 and 5000
-const min = 500
-const max = 5000
-const randomNumber = Math.floor(Math.random() * (max - min + 1)) + 500;
+const min = 10
+const max = 50
+const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
 // Math.random() returns a number between 0 and 1 (excluding 1), now to make the scale
 // from 500 to 5K, I found this formula: Math.floor(Math.random() * (max - min + 1)) + min
 // so now our range is (0, max - min + 1), and we add 500 to shift it to (min, max + 1) 
@@ -13,12 +13,11 @@ let sumbitButton = document.getElementById('sumbitButton');
 const playAgainButton = document.getElementById("playAgainButton");
 const winOrLoseBG = document.getElementById("customBackground");
 let previousGuessesArr = [];
-
 playAgainButton.style.visibility = "hidden";
 
 function processGuess(event) {
 
-    if (previousGuessesArr.length < 10) {
+    if (previousGuessesArr.length < 20) {
     let userGuess = Number(userInput.value); 
     previousGuessesArr.push(userGuess);
     renderAttempts();
@@ -36,8 +35,9 @@ function processGuess(event) {
         messageToUser.style.color = 'red';
     }
     if (userGuess === randomNumber) {
-        messageToUser.innerText = "Great! You Won!!";
+        messageToUser.innerText = " Great ! You Won ! !";
         messageToUser.style.color = 'white';
+        messageToUser.style.fontSize = '20px';
         winOrLoseBG.style.backgroundColor = 'green';
         messageToUser.style.padding = '10px';
         userInput.disabled = true;
@@ -47,17 +47,27 @@ function processGuess(event) {
 }
 else {
     messageToUser.innerText = " !! GAME OVER !!";
+	messageToUser.style.fontSize = '20px';
     messageToUser.style.color = 'white';
     winOrLoseBG.style.backgroundColor = 'red';
     messageToUser.style.padding = '10px';
     userInput.disabled = true;
     sumbitButton.disabled = true;
     playAgainButton.style.visibility = "visible";
+	dispalyRandomNumber();
 }
 }
 function renderAttempts() {
     previousGuesses.innerText = 'Previous guesses: ' + previousGuessesArr.join(' - ');
 }
+function dispalyRandomNumber() {
+    previousGuesses.innerText = `The number was: ${randomNumber}`;
+	previousGuesses.style.fontFamily = 'courier';
+	previousGuesses.style.fontSize = '22px';
+    previousGuesses.style.color = 'red';
+    previousGuesses.style.fontWeight = '700';
+}
 function refreshPage() {
     location.reload();
 }
+
